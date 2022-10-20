553973786e2d342825c5a88dc77708d337684a09
Céline <celine.github@martin-parisot.com>
2022-10-20 10:54:28 +0200

    structures

--- dictionary.h
+++ dictionary.h
@@ -4,114 +4,112 @@
 //verbes
 typedef struct s_mot_verbe
 {
-    char *temps;
-    char nombre;
-    char personne;
-    char *base;
-    char *mot;
+	char *temps;
+	char nombre;
+	char personne;
+	char *base;
+	char *mot;
 } t_mot_verbe;
 
 typedef struct s_verbe
 {
-    t_mot_verbe *mot;
-    struct s_verbe *next;
+	t_mot_verbe *mot;
+	struct s_verbe *next;
 } t_verbe;
 
 typedef struct s_node_verbe
 {
-    char lettre;
-    t_verbe *liste_flechie;
-    struct s_node_verbe *tab_node[26];
+	char lettre;
+	t_verbe *liste_flechie;
+	struct s_node_verbe *tab_node[26];
 } t_node_verbe;
 
 typedef struct s_tree_verbe
 {
-    t_node_verbe *root;
+	t_node_verbe *root;
 } t_tree_verbe;
 
-void    ft_init_verbe();
-t_tree_verbe   createTreeVerbe(t_node_verbe *node);
-t_node_verbe    *init_tab();
-t_node_verbe    *createNodeVerbe(char lettre);
+void			ft_init_verbe();
+t_tree_verbe	createTreeVerbe(t_node_verbe *node);
+t_node_verbe	*createNodeVerbe(char lettre);
 
-/*
 //adjectifs
-typedef struct s_tree_adj
+typedef struct s_mot_ajd
 {
-    t_node_adj *root;
-} t_tree_adj;
-
-typedef struct s_node_adj
-{
-    char lettre;
-    t_adj *liste_flechie;
-    t_node_adj tab_node[26];
-} t_node_adj;
+	char genre;
+	char nombre;
+	char *base;
+	char *mot;
+} t_mot_adj;
 
 typedef struct s_adj
 {
-    t_mot_adj *mot;
-    t_adj *next;
+	t_mot_adj *mot;
+	struct s_adj *next;
 } t_adj;
 
-typedef struct s_mot_ajd
+typedef struct s_node_adj
 {
-    char genre;
-    char nombre;
-    char *base;
-    char *mot;
-} t_mot_adj;
+	char lettre;
+	t_adj *liste_flechie;
+	struct s_node_adj *tab_node[26];
+} t_node_adj;
+
+typedef struct s_tree_adj
+{
+	t_node_adj *root;
+} t_tree_adj;
 
 //adverbes
-typedef struct s_tree_adv
+typedef struct s_mot_ajv
 {
-    t_node_adv *root;
-} t_tree_adv;
-typedef struct s_node_adv
-{
-    char lettre;
-    t_adv *liste_flechie;
-    t_node_adv tab_node[26];
-} t_node_adv;
+	char *base;
+	char *mot;
+} t_mot_adv;
 
 typedef struct s_adv
 {
-    t_mot_adv *mot;
-    t_adv *next;
+	t_mot_adv *mot;
+	struct s_adv *next;
 } t_adv;
 
-typedef struct s_mot_ajv
+typedef struct s_node_adv
 {
-    char *base;
-    char *mot;
-} t_mot_adv;
+	char lettre;
+	t_adv *liste_flechie;
+	struct s_node_adv *tab_node[26];
+} t_node_adv;
+
+typedef struct s_tree_adv
+{
+	t_node_adv *root;
+} t_tree_adv;
 
 //noms
-typedef struct s_tree_nom
+typedef struct s_mot_nom
 {
-    t_node_nom *root;
-} t_tree_nom;
-
-typedef struct s_node_nom
-{
-    char lettre;
-    t_nom *liste_flechie;
-    t_node_nom tab_node[26];
-} t_node_nom;
+	char genre;
+	char nombre;
+	char *base;
+	char *mot;
+} t_mot_nom;
 
 typedef struct s_nom
 {
-    t_mot_nom *mot;
-    t_nom *next;
+	t_mot_nom *mot;
+	struct s_nom *next;
 } t_nom;
 
-typedef struct s_mot_nom
+typedef struct s_node_nom
 {
-    char genre;
-    char nombre;
-    char *base;
-    char *mot;
-} t_mot_nom;
-*/
+	char lettre;
+	t_nom *liste_flechie;
+	struct s_node_nom *tab_node[26];
+} t_node_nom;
+
+typedef struct s_tree_nom
+{
+	t_node_nom *root;
+} t_tree_nom;
 
 #endif
